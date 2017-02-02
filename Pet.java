6787ccc3b0058db53d0292f317954d26802c8b46
lfern216 <lfern216@fiu.edu>
2017-02-01 22:00:22 -0500

    Add files via upload

---
+++ Pet.java
@@ -0,0 +1,31 @@
+package Kattis;
+
+import java.util.Scanner;
+
+public class Pet {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		int currentTotal = 0;
+		int location = -1;
+		int totalf = 0;
+		
+		for(int i=0;i<5;i++){
+			
+			currentTotal += in.nextInt();
+			currentTotal += in.nextInt();
+			currentTotal += in.nextInt();
+			currentTotal += in.nextInt();
+			
+			if(currentTotal > totalf){
+				totalf = currentTotal;
+				location = i+1;
+				currentTotal = 0;
+			}else{
+				currentTotal = 0;
+			}
+		}
+		System.out.println(location + " " + totalf);
+	}
+}
